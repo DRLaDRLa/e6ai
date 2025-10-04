acb32f7ca6f288a929eb17470a2c1364b8f87e79
Cinder <cinder@e621.net>
2025-10-04 08:43:10 -0700

    Migrate to a new route

--- config/routes.rb
+++ config/routes.rb
@@ -276,6 +276,8 @@ Rails.application.routes.draw do
       get :random
     end
     member do
+      get :report
+      post :report
       get :update_iqdb
       put :revert
       put :copy_notes
@@ -297,7 +299,7 @@ Rails.application.routes.draw do
     end
   end
   resources :post_events, only: :index
-  resources :post_flags, except: %i[destroy] do
+  resources :post_flags, except: %i[destroy new] do
     member do
       post :clear_note
     end
